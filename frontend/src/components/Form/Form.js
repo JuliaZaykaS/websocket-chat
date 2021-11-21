6ea441c481b8ab2f81fd0de1cbbfd09f37695681
JuliaZaykaS <sys55@mail.ru>
2021-11-21 14:56:32 +0300

    start

---
+++ frontend/src/components/Form/Form.js
@@ -0,0 +1,31 @@
+
+
+export const Form = () => {
+    return (
+        <form>
+            <label>
+                Введите имя
+                <input
+                type='text'
+                    name='message'
+                    value=''
+                    onChange={}
+                ></input>
+            </label>
+            <label>
+                Текст сообщения
+                <input
+                    type='text'
+                    name='message'
+                    value=''
+                    onChange={}
+
+                ></input>
+            </label>
+            <button type='submit' onClick={}>
+
+            </button>
+
+        </form>
+    )
+}
