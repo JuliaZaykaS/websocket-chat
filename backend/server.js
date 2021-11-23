c4c0270e83df82d9f43744833ca360d07f7b35fd
JuliaZaykaS <sys55@mail.ru>
2021-11-23 15:04:10 +0300

    predeploy

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 require('dotenv').config()
 const path = require('path')
 
-const PORT = process.env.PORT || 5000
+const react = path.join(__dirname, '../', 'frontend', 'build' );
+// console.log(__dirname);
+// console.log(react);
+
+const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(express.static(react))
 const http = require('http');
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
